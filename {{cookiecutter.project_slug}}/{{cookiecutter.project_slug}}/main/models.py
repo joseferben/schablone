b2e636623bd542cd35cfe38e4beb5b7eebdec298
Josef Erben <josef@erben.io>
2021-12-04 17:58:35 +0100

    Use correct bootstrap JS

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/models.py
@@ -2,8 +2,6 @@ import logging
 
 from django.db import models
 
-from {{cookiecutter.project_slug}}.contrib.mixins import EntityMixin
-
 logger = logging.getLogger(__name__)
 
 # Create your models here.
---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+app_name = "main"
+
+urlpatterns = [
+    # Add your routes here.
+]
