aa5be3a57cec5d5c0362563a45cd8961bb4fb7da
Josef Erben <josef@erben.io>
2021-12-08 17:42:17 +0100

    Add better example for receivers.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/receivers.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/receivers.py
@@ -1,7 +1,10 @@
 import logging
 
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 logger = logging.getLogger(__name__)
 
-# Handle your signals here.
+# @receiver(post_save, sender=Model)
+# def add_to_any_region(sender, **kwargs):
+#     pass
