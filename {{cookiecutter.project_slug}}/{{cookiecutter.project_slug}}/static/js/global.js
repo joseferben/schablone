c972e707b4970d99c88b2ee952c305877deadf6d
Josef Erben <josef@erben.io>
2022-01-12 21:58:02 +0100

    Load all JS deferred

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/static/js/global.js
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/static/js/global.js
@@ -1,10 +1,28 @@
 function loadTooltips() {
-  const tooltipTriggerList =
-        [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  const tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
 
-  const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-    return new bootstrap.Tooltip(tooltipTriggerEl);
+  const tooltipList = tooltipTriggerList
+        .map(function (tooltipTriggerEl) {
+          return new bootstrap.Tooltip(tooltipTriggerEl);
+        });
+}
+
+function loadHtmx() {
+  // TODO only load this when DEBUG=1
+  document.addEventListener("DOMContentLoaded", function(event) {
+    htmx.logger = function(elt, event, data) {
+      if(console) {
+        console.debug(event, elt, data);
+      }
+    };
+  });
+
+  document.body.addEventListener('htmx:configRequest', (event) => {
+    event.detail.headers['X-CSRFToken'] = '{{ csrf_token }}';
   });
 }
 
 loadTooltips();
+loadHtmx();
