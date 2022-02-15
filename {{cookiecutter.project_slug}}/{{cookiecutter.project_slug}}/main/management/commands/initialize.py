d78fd148e0fe4d492778ed07d74481020026928e
Josef Erben <josef@erben.io>
2022-02-15 15:13:10 +0100

    Fix initialize command

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/management/commands/initialize.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/main/management/commands/initialize.py
@@ -1,17 +1,16 @@
 from django.core.management.base import BaseCommand
 
 from django.contrib.sites.models import Site
+from django.contrib.auth.models import User
+
 
 class Command(BaseCommand):
-    help = 'Creates default site in the sites app and a default admin user'
+    help = "Creates default site in the sites app and a default admin user"
 
     def handle(self, *args, **kwargs):
         Site.objects.create(
-            domain='{{cookiecutter.domain_name}}',
-            name='{{cookiecutter.domain_name}}'
+            domain="{{cookiecutter.domain_name}}", name="{{cookiecutter.domain_name}}"
         )
         User.objects.create(
-            email='admin@example.com',
-            username='admin',
-            password='password'
+            email="admin@example.com", username="admin", password="password"
         )
