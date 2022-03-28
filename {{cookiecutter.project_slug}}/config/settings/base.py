3931dfcedb8e734279500ee1302d8ce0e1da46ed
Josef Erben <josef@erben.io>
2022-03-28 05:06:31 +0200

    Fix base.py linting issues

--- {{cookiecutter.project_slug}}/config/settings/base.py
+++ {{cookiecutter.project_slug}}/config/settings/base.py
@@ -314,13 +314,9 @@ ACCOUNT_ADAPTER = "{{cookiecutter.project_slug}}.users.adapters.AccountAdapter"
 # https://django-allauth.readthedocs.io/en/latest/forms.html
 ACCOUNT_FORMS = {"signup": "{{cookiecutter.project_slug}}.users.forms.UserSignupForm"}
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-SOCIALACCOUNT_ADAPTER = (
-    "{{cookiecutter.project_slug}}.users.adapters.SocialAccountAdapter"
-)
+SOCIALACCOUNT_ADAPTER = ("{{cookiecutter.project_slug}}.users.adapters.SocialAccountAdapter")
 # https://django-allauth.readthedocs.io/en/latest/forms.html
-SOCIALACCOUNT_FORMS = {
-    "signup": "{{cookiecutter.project_slug}}.users.forms.UserSocialSignupForm"
-}
+SOCIALACCOUNT_FORMS = {"signup": "{{cookiecutter.project_slug}}.users.forms.UserSocialSignupForm"}
 # django-compressor
 # ------------------------------------------------------------------------------
 # https://django-compressor.readthedocs.io/en/latest/quickstart/#installation
