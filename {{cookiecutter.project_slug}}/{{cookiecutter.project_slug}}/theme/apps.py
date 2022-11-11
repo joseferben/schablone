ca300d844b7cab6a4c478a06e43bbf54e9522669
Josef Erben <josef@erben.systems>
2022-11-11 20:20:21 +0700

    Fix app name

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/theme/apps.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/theme/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ThemeConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "theme"
+    name = "{{cookiecutter.project_slug}}.theme"
