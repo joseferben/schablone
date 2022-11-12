596b674a3124c66859b0f86a1a0b69ece26d2820
Josef Erben <josef@erben.systems>
2022-11-12 13:44:17 +0700

    Implement navigation JS

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/static/js/project.js
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/static/js/project.js
@@ -1,1 +1,40 @@
-/* Project specific Javascript goes here. */
+//* Project specific Javascript goes here. */
+
+function toggleMobileNav(evt) {
+  const mobileNav = document.getElementById("mobileNav");
+  if (mobileNav) {
+    if (mobileNav.dataset.state === "open") {
+      // Open -> Close
+      mobileNav.classList.remove(
+        "duration-150",
+        "ease-out",
+        "opacity-100",
+        "scale-100"
+      );
+      mobileNav.classList.add(
+        "transition",
+        "duration-100",
+        "ease-in",
+        "opacity-0",
+        "scale-95"
+      );
+      mobileNav.dataset.state = "closed";
+    } else {
+      // Close -> Open
+      mobileNav.classList.remove(
+        "duration-100",
+        "ease-in",
+        "opacity-0",
+        "scale-95"
+      );
+      mobileNav.classList.add(
+        "transition",
+        "duration-150",
+        "ease-out",
+        "opacity-100",
+        "scale-100"
+      );
+      mobileNav.dataset.state = "open";
+    }
+  }
+}
