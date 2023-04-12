ce79ff4a0c719fafd2c8c8d846798ef70cd12cf3
Josef Erben <josef@erben.systems>
2023-04-12 18:00:50 +0200

    Use Django contrib apps for static files and admin

--- {{cookiecutter.project_slug}}/config/settings/test.py
+++ {{cookiecutter.project_slug}}/config/settings/test.py
@@ -12,3 +12,5 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
