a092651ae66b1c46cf7e1e95d16338cb4cfca8db
Josef Erben <josef@erben.systems>
2023-04-14 19:25:54 +0200

    Fix formatting

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
 
 from {{cookiecutter.project_slug}}.users.forms import (
-    UserAdminChangeForm, 
+    UserAdminChangeForm,
     UserAdminCreationForm,
 )
 
