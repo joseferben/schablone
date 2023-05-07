6f20e494c5057a2bc5942162ea408950a90d7120
Josef Erben <josef@erben.systems>
2023-05-07 18:43:02 +0200

    make sync

---
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sync/management/commands/sync_local_site.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.contrib.site.models import Site
+
+
+class Command(BaseCommand):
+    help = "Gets the first Site and adjusts the host and port."
+
+    def handle(self, *args, **options):
+        if settings.DEBUG:
+            print("Adjusting Site")
+            s = Site.objects.get(hostname="{{cookiecutter.domain}}")
+            s.hostname = "localhost"
+            s.port = 8000
+            s.save()
+        else:
+            raise Exception("This command can only be run in DEBUG mode.")
