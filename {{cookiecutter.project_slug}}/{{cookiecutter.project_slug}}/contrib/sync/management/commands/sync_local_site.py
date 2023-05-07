471c35f0d9cccdfd4b533e52f7f9ad055be19bb4
Josef Erben <josef@erben.systems>
2023-05-07 18:48:41 +0200

    fix site model

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sync/management/commands/sync_local_site.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/contrib/sync/management/commands/sync_local_site.py
@@ -1,17 +1,16 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from django.contrib.site.models import Site
+from django.contrib.sites.models import Site
 
 
 class Command(BaseCommand):
-    help = "Gets the first Site and adjusts the host and port."
+    help = "Gets the project Site and adjusts the domain."
 
     def handle(self, *args, **options):
         if settings.DEBUG:
             print("Adjusting Site")
-            s = Site.objects.get(hostname="{{cookiecutter.domain_name}}")
-            s.hostname = "localhost"
-            s.port = 8000
+            s = Site.objects.get(domain="www.{{cookiecutter.domain_name}}")
+            s.domain = "localhost"
             s.save()
         else:
             raise Exception("This command can only be run in DEBUG mode.")
