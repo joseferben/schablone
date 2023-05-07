c73aa3fe6db63b41228a26f7d4ded329ce465f3f
Josef Erben <josef@erben.systems>
2023-05-07 19:00:34 +0200

    formatting fixes

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/conftest.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/conftest.py
@@ -2,9 +2,9 @@ import pytest
 from faker import Faker
 
 from {{cookiecutter.project_slug}}.users.models import User
-from {{cookiecutter.project_slug}}.users.tests.factories import UserFactory
 
 fake = Faker()
+
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
@@ -14,4 +14,3 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user() -> User:
     return User(email=fake.email(), name=fake.name())
-
