ba668316c457c5c57bad8647751a4371ec082e8f
Josef Erben <josef@erben.systems>
2023-05-07 19:04:08 +0200

    remove references to factory-boy

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_tasks.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_tasks.py
@@ -1,12 +1,15 @@
 import pytest
+from faker import Faker
 
+from {{cookiecutter.project_slug}}.users.models import User
 from {{cookiecutter.project_slug}}.users.tasks import get_users_count
-from {{cookiecutter.project_slug}}.users.tests.factories import UserFactory
 
 pytestmark = pytest.mark.django_db
-
+fake = Faker()
 
 def test_user_count(settings):
     """A basic test to execute the get_users_count task."""
-    UserFactory.create_batch(3)
+    User.objects.create(email=fake.email())
+    User.objects.create(email=fake.email())
+    User.objects.create(email=fake.email())
     assert get_users_count()() == 3  # type: ignore
