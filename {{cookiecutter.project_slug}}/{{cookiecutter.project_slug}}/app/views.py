c2596576dceb940c8fe9631254e14748ad3f8cb0
Josef Erben <josef@erben.systems>
2023-05-08 13:03:17 +0200

    email_login

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/views.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/app/views.py
@@ -1,5 +1,6 @@
 from django import http
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from {{cookiecutter.project_slug}}.users.models import User
 
@@ -8,5 +9,5 @@ class AuthedHttpRequest(http.HttpRequest):
     user: User  # type: ignore [assignment]
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "app/index.html"
