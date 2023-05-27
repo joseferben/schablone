4abc0fc8817959c4dfad7a1142a5cff8e6f5e489
Josef Erben <josef@erben.systems>
2023-05-27 17:24:08 +0200

    implement simple settings page

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
